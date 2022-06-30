78e8ccdb88dade61ee3471f8b2f56eb2e6edda33
ifty03 <ashikulislamb84@gmail.com>
2022-06-30 21:00:00 +0600

    modal added

--- src/Pages/Todo/TaskModal.js
+++ src/Pages/Todo/TaskModal.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 const TaskModal = () => {
+  const handelTask = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const description = e.target.description.value;
+    const date = e.target.date.value;
+    console.log(name, description, date);
+    e.target.reset();
+  };
   return (
     <div className="bg-slate-500">
       {/* <!-- The button to open modal --> */}
@@ -10,12 +18,12 @@ const TaskModal = () => {
         <div class="modal-box bg-slate-700 ">
           <label
             for="my-modal-6"
-            class="btn btn-sm btn-circle absolute right-2 top-2"
+            class="btn btn-sm btn-circle absolute right-2 top-2 bg-gradient-to-t from-purple-500 to-pink-600 hover:bg-gradient-to-t hover:from-pink-500 hover:to-purple-600 text-white"
           >
             ✕
           </label>
           {/* from input */}
-          <form>
+          <form onSubmit={handelTask}>
             <div class="form-control">
               <label class="label">
                 <span class="label-text">Task Title</span>
@@ -24,6 +32,18 @@ const TaskModal = () => {
                 type="text"
                 name="name"
                 placeholder="Task Title"
+                required
+                class="input input-bordered"
+              />
+            </div>
+            <div class="form-control">
+              <label class="label">
+                <span class="label-text">Date</span>
+              </label>
+              <input
+                type="text"
+                name="date"
+                placeholder="Date"
                 required
                 class="input input-bordered"
               />
@@ -39,12 +59,14 @@ const TaskModal = () => {
                 placeholder="description"
               ></textarea>
             </div>
+            <div class="modal-action">
+              <input
+                className="btn bg-gradient-to-t from-purple-500 to-pink-600 hover:bg-gradient-to-t hover:from-pink-500 hover:to-purple-600 border-0 text-white"
+                type="submit"
+                value="ADD!"
+              />
+            </div>
           </form>
-          <div class="modal-action">
-            <label for="my-modal-6" class="btn">
-              ADD!
-            </label>
-          </div>
         </div>
       </div>
     </div>
